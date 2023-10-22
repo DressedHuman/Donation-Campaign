d69cabd7162de73a43dd9220ec13eca5304eb725
DressedHuman <dressed.human@gmail.com>
2023-10-23 04:46:06 +0600

    added warning message for the donation page with no donations

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -2,24 +2,30 @@ import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getDonatedItemIds } from "../../utilities/LocalStorage";
 import Donation from "../Donation/Donation";
+import NotFound from "../NotFound/NotFound";
 
 const Donations = () => {
     const [donations, setDonations] = useState([]);
     const allDonationItems = useLoaderData();
 
-    useEffect(()=>{
+    useEffect(() => {
         const donatedItemIds = getDonatedItemIds();
         const donatedItems = allDonationItems.filter(donationItem => donatedItemIds.includes(donationItem.id));
 
         setDonations(donatedItems);
-    },[])
+    }, [])
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mx-[31px]">
+        <>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mx-[31px]">
+                {
+                    donations.map(donation => <Donation key={donation.id} donationItem={donation} />)
+                }
+            </div>
             {
-                donations.map(donation => <Donation key={donation.id} donationItem={donation} />)
+                !donations.length && <NotFound not_found_text={"You haven't donated yet!"} />
             }
-        </div>
+        </>
     );
 };
 
