06565575238015b9de382b6c2ef0bc6a7c6a960b
DressedHuman <dressed.human@gmail.com>
2023-10-23 14:58:39 +0600

    added the functional see all button on the donations page

--- src/components/Donations/Donations.jsx
+++ src/components/Donations/Donations.jsx
@@ -7,6 +7,7 @@ import { DisplayDataContext } from "../Root/Root";
 const Donations = () => {
     const [donations, setDonations] = useState([]);
     const { donationItems } = useContext(DisplayDataContext);
+    const [displayItemsLength, setDisplayItemsLength] = useState(4);
 
     useEffect(() => {
         const donatedItemIds = getDonatedItemIds();
@@ -19,11 +20,16 @@ const Donations = () => {
         <>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mx-[31px]">
                 {
-                    donations.map(donation => <Donation key={donation.id} donationItem={donation} />)
+                    donations.slice(0, displayItemsLength).map(donation => <Donation key={donation.id} donationItem={donation} />)
                 }
             </div>
             {
                 !donations.length && <NotFound not_found_text={"You haven't donated yet!"} />
+            }
+            {
+                donations.length > 4 && displayItemsLength === 4 && <div className="text-center mt-10">
+                    <button onClick={()=>setDisplayItemsLength(donations.length)} className="btn btn-primary border-none px-7 py-4 rounded-lg bg-[#009444] text-white font-semibold">See All</button>
+                </div>
             }
         </>
     );
